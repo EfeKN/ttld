680574ee7effd439c5e46a420d6e47ab9132cbc1
fenomen9000 <fenomen9000@gmail.com>
2022-04-01 23:08:43 +0300

    experimenting game loop  #1

--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -6,11 +6,16 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-public class GameScreen extends JPanel {
+public class GameScreen extends JPanel implements Runnable {
     
     //private BufferedImage img;
     private ArrayList<BufferedImage> sprites;
-    Random rand;
+    private Random rand;
+    private long frames;
+    private long lastTime;
+    private double timePerFrame;
+    private  long lastFrame;
+    Thread gameThread;
 
     public GameScreen(BufferedImage img) {
         rand = new Random();
@@ -22,14 +27,49 @@ public class GameScreen extends JPanel {
         }
     }
 
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+        
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         for(int i = 1; i <=18; i++) {
             for(int j = 1; j <= 32; j++) {
                 g.drawImage(sprites.get(rand.nextInt(64)), (j*32)-32, (i*32)-32, null);
             }
+        } 
+        //callFPS();
+    }
+
+    public void loopGame() {
+        if(System.nanoTime() - lastFrame >= timePerFrame) {
+            lastFrame = System.nanoTime();
+            repaint();
         }
-        repaint();
+    }
+
+    public void callFPS() {
+        frames++;
+        if(System.currentTimeMillis() - lastTime >= 1000) {
+            System.out.println("Frame per second " + frames);
+            frames = 0;
+            lastTime = System.currentTimeMillis();
+        }
+    }
+
+    @Override
+    public void run() {
+        while(gameThread!=null) {
+            update();
+            repaint();
+        }
+        
+    }
+
+    public void update() {
+
     }
 
 }
